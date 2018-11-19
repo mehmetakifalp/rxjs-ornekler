07dd76bb8ba868e5e7763edf558729186cfa742e
mehmetakifalp <mehmetakifalp@gmail.com>
2018-11-19 14:48:26 +0300

    share click event 2 subscriber

--- src/app/content/share/share.component.ts
+++ src/app/content/share/share.component.ts
@@ -1,55 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { of, interval, Observable, Subject } from 'rxjs';
-import { map, share, tap, mapTo, take, mergeMap, combineLatest } from 'rxjs/operators';
+import { Component, OnInit, ViewChild, AfterContentInit, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-share',
   templateUrl: './share.component.html',
   styleUrls: ['./share.component.scss']
 })
-export class ShareComponent implements OnInit {
+export class ShareComponent implements AfterViewInit {
 
   dataSource = of({name:'Mehmet', company:'Arneca Teknoloji', title:'Sr. Software Developer'});
 
   constructor() { }
 
-  ngOnInit() {
 
-
-    /* const sharedObserver = this.dataSource.pipe(share());
-
-    const example1 = sharedObserver.subscribe( a => {
-      console.log("shared example1");
-      console.log(a);
-    });
-
-    const example2 = sharedObserver.subscribe( a => {
-      console.log("shared example2");
-      console.log(a);
-    }); */
-
-    /*
-
-    const interval$ = interval(1000)
-  .pipe(
-     tap(console.log),
-     mapTo('nextValue'),
-     take(2),
-     share(),
-  );
-
-
-  const subscription = interval$.subscribe(console.log);
-
-  setTimeout(() => {
-    subscription.unsubscribe();
-  }, 5000);
-
-    */
-
-}
-
-objectKeys = Object.keys;
 
 
 
@@ -63,13 +27,26 @@ notifyData = of([{
 }]);
 
 
+@ViewChild('app') myApp;
+
+
+ngAfterViewInit() {
+    const click$ = fromEvent(this.myApp, 'click');
+    const sub1 = click$.subscribe(val => {
+      console.log('sub1:', val);
+    });
 
 
 
-
-addNewNotification(newData){
-
+    const sub2 = click$.subscribe(val => {
+      console.log('sub2:', val);
+    });
 
 }
+
+
+
+
+
 
 }
