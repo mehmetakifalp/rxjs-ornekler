de9ef50292f66ee72d2b551ef4bae06ec172acf1
mehmetakifalp <mehmetakifalp@gmail.com>
2018-11-20 17:32:20 +0300

    merge

---
+++ src/app/content/merge/merge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-merge',
+  templateUrl: './merge.component.html',
+  styleUrls: ['./merge.component.scss']
+})
+export class MergeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
