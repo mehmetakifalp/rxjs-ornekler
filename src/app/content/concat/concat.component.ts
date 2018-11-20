c02f730b3759d0e431de5ea1d8917cf7267bff81
mehmetakifalp <mehmetakifalp@gmail.com>
2018-11-20 16:13:24 +0300

    concat rxjs

---
+++ src/app/content/concat/concat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-concat',
+  templateUrl: './concat.component.html',
+  styleUrls: ['./concat.component.scss']
+})
+export class ConcatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
